3fab8dd0a5acc2bc7325e0b5e6a14370b44690e1
andre.aguiar <andre.aguiar@bevicred.com.br>
2024-04-11 13:38:23 -0300

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import styles from "./page.module.css";
+
 const Home = () => {
   return (
     <main>
       <section>
         <div className="container-sm">
-          <h1>Hello World</h1>
+          <h1 className={styles.title}>Hello World</h1>
         </div>
       </section>
     </main>
